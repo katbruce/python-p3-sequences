e1e2d7ba455070bdf99037e18d9b82d74cbf61a9
Kathryn <kathrynbruce7@gmail.com>
2023-05-02 11:52:29 -0600

    lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
 
 def print_fibonacci(length):
-    pass
+    fibo = list()
+    for i in range(length):
+       fibo.append(recur_fibo(i))
+    print(fibo)
